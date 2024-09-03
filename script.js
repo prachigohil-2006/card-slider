070c0537e0af940021012097959ad879a49efa0e
prachigohil-2006 <prachigohil201@gmail.com>
2024-09-03 16:07:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.slider-wrapper', {
+    
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    //responsive breakpoints
+    breakpoints:{
+        0: {
+            slidesPerView: 1
+        },
+        620: {
+            slidePerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+  
+    
+  });
